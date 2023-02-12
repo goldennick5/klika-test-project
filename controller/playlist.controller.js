6737952f76d91cc8286c68261ca12623105348a5
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-02-12 17:54:19 +0600

    wef

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -6,8 +6,7 @@ const devConfig = {
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-    address: process.env.PG_ADDRESS
+    port: process.env.PG_PORT
 }
 
 const proConfig = {
