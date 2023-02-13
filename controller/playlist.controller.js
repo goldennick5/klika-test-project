702788469a9de7b22b25739140921a5bc55a4e7d
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-02-13 14:23:53 +0600

    comment in database.sql

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -1,22 +1,32 @@
 const Pool = require('pg').Pool; //1
 require('dotenv').config();
 
-const devConfig = {
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-}
+// const devConfig = {
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT,
+// }
+//
+// const proConfig = {
+//     connectionString: process.env.DATABASE_URL,
+// }
+// //process.env.NODE_ENV === "production" ? proConfig : devConfig
+// console.log(proConfig)
+// const pool = new Pool(
+//     process.env.NODE_ENV === "production" ? proConfig : devConfig
+// );
 
-const proConfig = {
-    connectionString: process.env.DATABASE_URL,
-}
-//process.env.NODE_ENV === "production" ? proConfig : devConfig
-console.log(proConfig)
-const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction,
+})
 
 class PlaylistController {
 
