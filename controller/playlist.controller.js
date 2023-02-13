1b1a83678778e8129716195b393ab6b2180c3952
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-02-13 16:47:34 +0600

    wef

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -11,9 +11,11 @@ const devConfig = {
 
 const proConfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 }
+
 //process.env.NODE_ENV === "production" ? proConfig : devConfig
-console.log(process.env.NODE_ENV)
+
 const pool = new Pool(
     process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
